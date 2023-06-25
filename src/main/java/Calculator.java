34be6fc2dd1e9f6af5a089eb45c3706733a40a2b
Sevlaria <Sevlaria@yandex.ru>
2023-06-25 15:58:13 +0300

    Консольное приложение Бычковой Дарьи(исправленное)

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,41 +3,36 @@ import java.util.Scanner;
 
 public class Calculator {
     String listOfProducts = "Добавленные товары:\n";
-    Double coast;
+    Double cost;
     Double sum = 0.0;
     String product;
     boolean t;
 
     public void calculate(int countOfClient) {
         while (true) {
-           Scanner scanner = new Scanner(System.in);
-            System.out.println("Какой товары вы желаете положить в корзину?");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Какой товар вы желаете положить в корзину?");
             product = scanner.nextLine();
             while (t == false) {
-               t = input();
-                }
-
+                t = input();
+            }
             t = false;
-
-                listOfProducts = listOfProducts +  product + "  " + coast + " \n";
-                sum = sum + coast;
-                System.out.println("Вы успешно добавили товар в корзину!" +
-                        "Хотите ли вы добавить еще один товар?\n Введите любой символ, чтобы продолжить покупки. " +
-                        "Для завершения покупок наберите команду 'завершить'!");
-                product = scanner.nextLine();
-                if (product.equalsIgnoreCase("завершить")) {
-                    output(countOfClient);
-                    return;
-                }
-
-
-
+            listOfProducts = listOfProducts + String.format("%s   %.2f\n", product, cost);
+            sum = sum + cost;
+            System.out.println("Вы успешно добавили товар в корзину!" +
+                    "Хотите ли вы добавить еще один товар?\n Введите любой символ, чтобы продолжить покупки. " +
+                    "Для завершения покупок наберите команду 'завершить'!");
+            product = scanner.nextLine();
+            if (product.equalsIgnoreCase("завершить")) {
+                output(countOfClient);
+                return;
+            }
         }
     }
 
     public void output(int countOfClient) {
         System.out.println(listOfProducts);
-        sum = sum/countOfClient;
+        sum = sum / countOfClient;
         switch ((int) Math.floor(sum)) {
             case 1:
                 System.out.println("Общая сумма покупки в расчете на одного человека: "
@@ -53,22 +48,19 @@ public class Calculator {
                 System.out.println("Общая сумма покупки в расчете на одного человека: "
                         + String.format("%.2f", sum) + " рублей");
                 break;
-
         }
-
     }
 
-   public boolean input(){
+    public boolean input() {
         System.out.println("Введите стоимость товара.");
         try {
             Scanner in = new Scanner(System.in);
-            coast = in.nextDouble();
+            cost = in.nextDouble();
             return true;
         } catch (InputMismatchException e) {
             System.out.println("Введено некорректное значение цены, " +
                     "введите в формате рубли,копейки.");
             return false;
         }
-        }
-
     }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать, на сколько человек необходимо разделить счет?");
-
         while (true) {
             Scanner scanner = new Scanner(System.in);
             try {
@@ -13,20 +12,15 @@ public class Main {
                     System.out.println("Простите, вы ввели некорректное значение, введите количество человек еще раз!");
                 } else if (countOfClient == 1) {
                     System.out.println("Ваш счет делить не нужно! Введите корректное значение!");
-
                 } else {
                     Calculator calculator = new Calculator();
                     calculator.calculate(countOfClient);
                     scanner.close();
                     break;
                 }
-
             } catch (InputMismatchException e) {
                 System.out.println("Введено некорректное значение, попробуйте ввести значение еще раз!");
             }
-
         }
-
-
     }
 }
